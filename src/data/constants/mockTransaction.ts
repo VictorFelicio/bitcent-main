c2ad8fc880d4320b9f836e52e045436ac5c9a16f
Victor Felicio <victor_felicio@hotmail.com>
2025-01-04 20:17:00 -0300

    feat: modify name os archives

--- src/data/constants/mockTransaction.ts
+++ src/data/constants/mockTransaction.ts
@@ -1,34 +1,34 @@
 import { Id } from "@/logic/core/common/Id";
-import { Transaction } from "@/logic/core/finances/Transaction";
-import { TransactionType } from "@/logic/core/finances/TransactionType";
+import { Transaction } from "@/logic/interface/Transaction";
+import { TransactionEnum } from "@/logic/core/finances/enum/TransactionType";
 
 export const mockTransactions: Transaction[] = [
 	{
 		id: Id.genereateID(),
 		description: "Burger King Largo Treze",
 		value: 50.75,
-		date: new Date("2023-01-15"),
-		type: TransactionType.SPENDING,
+		date: new Date("2023-01-01"),
+		type: TransactionEnum.SPENDING,
 	},
 	{
 		id: Id.genereateID(),
-		description: "Salario",
+		description: "Salário",
 		value: 1500.0,
 		date: new Date("2023-01-01"),
-		type: TransactionType.EARNINGS,
+		type: TransactionEnum.EARNINGS,
 	},
 	{
 		id: Id.genereateID(),
-		description: "C0nta de Luz",
+		description: "Conta de Luz",
 		value: 75.2,
 		date: new Date("2023-01-10"),
-		type: TransactionType.SPENDING,
+		type: TransactionEnum.SPENDING,
 	},
 	{
 		id: Id.genereateID(),
 		description: "Freelance projeto",
 		value: 300.0,
 		date: new Date("2023-01-20"),
-		type: TransactionType.SPENDING,
+		type: TransactionEnum.SPENDING,
 	},
 ];
--- src/logic/interface/Transaction.ts
+++ src/logic/interface/Transaction.ts
@@ -1,9 +1,9 @@
-import { TransactionType } from "./TransactionType";
+import { TransactionEnum } from "../core/finances/enum/TransactionType";
 
 export interface Transaction {
 	id?: string;
 	description: string;
 	value: number;
 	date: Date;
-	type: TransactionType;
+	type: TransactionEnum;
 }
