921779a05ec5573b35f3c339d0321c57c55ab1f0
Victor Felicio <victor_felicio@hotmail.com>
2025-01-25 21:10:19 -0300

    feat: create auth features

--- src/logic/firebase/auth/Authentication.ts
+++ src/logic/firebase/auth/Authentication.ts
@@ -1,7 +1,13 @@
-import { Auth, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { Auth, getAuth, onIdTokenChanged, signInWithPopup, signOut } from "firebase/auth";
 import { app } from "../config/app";
 import { User } from "@/logic/interface/Usuario";
-import { GoogleAuthProvider } from "firebase/auth/web-extension";
+import {
+	GoogleAuthProvider,
+	User as UserFirebaseInterface,
+} from "firebase/auth/web-extension";
+
+export type MonitorinUser = (user: User | null) => void;
+export type CancelMonitorinUser = () => void;
 export class Authtentication {
 	private _auth: Auth;
 
@@ -10,11 +16,31 @@ export class Authtentication {
 	}
 
 	async loginGoogle(): Promise<User | null> {
-		const resp = signInWithPopup(this._auth, new GoogleAuthProvider());
-		await resp.user;
+		const resp = await signInWithPopup(this._auth, new GoogleAuthProvider());
+		return this.convertToUser(resp.user);
 	}
 
 	async logout(): Promise<void> {
 		await signOut(this._auth);
+	}
+
+	monitoring(notify: MonitorinUser): CancelMonitorinUser {
+		return onIdTokenChanged(this._auth, async (userFirabase) => {
+			const user = this.convertToUser(userFirabase);
+			notify(user);
+		});
+	}
+
+	private convertToUser(userFirabase: UserFirebaseInterface | null): User | null {
+		if (userFirabase?.email) return null;
+
+		const nameFirebase = userFirabase.email!.split("@")[0];
+
+		return {
+			id: userFirabase.uid,
+			name: userFirabase.displayName ?? nameFirebase,
+			email: userFirabase.email,
+			imageUrl: userFirabase.photoURL,
+		};
 	}
 }
